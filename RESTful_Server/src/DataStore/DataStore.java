a9fc0c85fb735f7fd72f79d7669a2519e048e56e
Lewis McLean <mclean.lewis@gmail.com>
2011-11-12 23:01:45 +0000

    added JSON library and SQL to JSON class i pinched from somewhere to modify

--- RESTful_Server/src/DataStore/DataStore.java
+++ RESTful_Server/src/DataStore/DataStore.java
@@ -11,23 +11,13 @@ import java.util.concurrent.*;
  * @author  lewismclean
  */
 public class DataStore  {
-	/**
-	 * @uml.property  name="requestQueue"
-	 * @uml.associationEnd  multiplicity="(0 -1)" elementType="DataStore.Request"
-	 */
+
 	private Queue requestQueue = new ConcurrentLinkedQueue();
-	/**
-	 * @uml.property  name="responseQueue"
-	 * @uml.associationEnd  multiplicity="(0 -1)" elementType="DataStore.Response"
-	 */
+
 	private Queue responseQueue = new ConcurrentLinkedQueue();
-	/**
-	 * @uml.property  name="available"
-	 */
+
 	private boolean available = false;
-	/**
-	 * @uml.property  name="threadNumber"
-	 */
+
 	private long threadNumber = 0;
 
 	public DataStore() {
@@ -57,9 +47,7 @@ public class DataStore  {
 		return null;
 	}
 	
-	/* (non-Javadoc)
-	 * @see DataStore.DataStoreInterface#getResponse()
-	 */
+
 
 	public synchronized Response getResponse() {
 		//notifyAll();
@@ -72,26 +60,21 @@ public class DataStore  {
 		return null;
 	}
 	
-	/* (non-Javadoc)
-	 * @see DataStore.DataStoreInterface#peekResponse()
-	 */
+
 	
 	public synchronized long peekResponse() {
-		
-
-		notifyAll();
+	
+		//notifyAll();
 		if(!responseQueue.isEmpty()){
 		return (long) ((Response) responseQueue.peek()).getOwnerThread();
 		}
 		else{return -1;}
 	}
-	
-	/* (non-Javadoc)
-	 * @see DataStore.DataStoreInterface#getThreadNumber()
-	 */
+
 	public synchronized long getThreadNumber(){
 		//notifyAll();
 		threadNumber++;
+		if(threadNumber>10000)threadNumber=0;
 		return threadNumber;
 		
 	}
---
+++ RESTful_Server/src/Tools/ResultSetToJson.java
@@ -0,0 +1,96 @@
+package Tools;
+
+import java.sql.SQLException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+//Copied from web somewhere, stackover-flow or something - will probably reuse somewhere to format the JSON
+//use the .jar I've compiled in the shared dropbox folder.
+
+public class ResultSetToJson {
+
+	public ResultSetToJson(){}
+	
+	public JSONArray convertResultSetToJSON(java.sql.ResultSet rs){
+
+        JSONArray json = new JSONArray();
+
+        try {
+
+             java.sql.ResultSetMetaData rsmd = rs.getMetaData();
+
+             while(rs.next()){
+                 int numColumns = rsmd.getColumnCount();
+                 JSONObject obj = new JSONObject();
+
+                 for (int i=1; i<numColumns+1; i++) {
+
+                     String column_name = rsmd.getColumnName(i);
+
+                     if(rsmd.getColumnType(i)==java.sql.Types.ARRAY){
+                         obj.put(column_name, rs.getArray(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.BIGINT){
+                         obj.put(column_name, rs.getInt(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.BOOLEAN){
+                         obj.put(column_name, rs.getBoolean(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.BLOB){
+                         obj.put(column_name, rs.getBlob(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.DOUBLE){
+                         obj.put(column_name, rs.getDouble(column_name)); 
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.FLOAT){
+                         obj.put(column_name, rs.getFloat(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.INTEGER){
+                         obj.put(column_name, rs.getInt(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.NVARCHAR){
+                         obj.put(column_name, rs.getNString(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.VARCHAR){
+                         obj.put(column_name, rs.getString(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.TINYINT){
+                         obj.put(column_name, rs.getInt(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.SMALLINT){
+                         obj.put(column_name, rs.getInt(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.DATE){
+                         obj.put(column_name, rs.getDate(column_name));
+                     }
+                     else if(rsmd.getColumnType(i)==java.sql.Types.TIMESTAMP){
+                        obj.put(column_name, rs.getTimestamp(column_name));   
+                     }
+                     else{
+                         obj.put(column_name, rs.getObject(column_name));
+                     } 
+
+                    }//end foreach
+                 json.put(obj);
+
+             }//end while
+
+
+
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+        return json;
+    }
+
+	
+}
